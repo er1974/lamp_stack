1059c08dbb24af7748500810f25995c61391e0a0
er1974 <ernegash@yahoo.com>
2015-02-10 20:23:43 -0800

    new ways

--- Survey_form/application/controllers/surveys.php
+++ Survey_form/application/controllers/surveys.php
@@ -4,34 +4,26 @@ class surveys extends CI_Controller
 {
 	public function index()
 	{
-		$this->load->view('survey_form');
+		$this->load->view('Survey_form');
 	}
 
 	public function process_form()
 	{
-	 	if($this->session->userdata('count') == NULL)
+	 	if($this->session->userdata('count')==null)
 	 	{
 	 		$this->session->set_userdata('count',1);
 	 		$result = $this->session->userdata('count');
-	 		$view_data['count'] = $result;
+	 		$POST['count'] = $result;
 	 	}
 	 	else
 	 	{
-	 		$view_count = $this->session->userdata('count');
-	 		$view_count += 1;
-
-	 		$this->session->set_userdata('count', $view_count);
-
-	 		$view_data['count'] = $this->session->userdata('count');;
+	 		$temp = $this->session->userdata('count');
+	 		$temp += 1;
+	 		$this->session->set_userdata('count',$temp);
+	 		$view_data['count'] = $result;
 	 	}
-
-	 	$view_data = $this->session->post();
-
- 		// $view_data['name'] 	   = $this->session->post('name');
- 		// $view_data['location'] = $this->input->post("location");
- 		// $view_data['language'] = $this->input->post("language");
- 		// $view_data['comment'] = $this->input->post("comment");
-
- 		$this->load->view('survey_result', $view_data);
+	 		$view_data[] = $this->session->post();
+	 		$this->load->view('survey_result', $view_data);
+	 	
 	}
 }
